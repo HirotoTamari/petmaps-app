34e5e83df0f80c83f5539bdb4671bedc0d5d35d2
Tamari Hiroto <henl16101118@gmail.com>
2020-08-05 14:59:25 +0900

    Update application

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+routes = ["飲食店", "病院", "ホテル", "トリミング"]
+routes.each do |route|
+    Route.create(name: route)
+end
+
+User.create(name: "first", email: "aa", image_name: "/default_user.jpg", password: "12345678", route1_id: 1, route2_id: 2, route3_id: 3)
