d8cf1acb4956e8d0635823a3080ce14a02674795
Tamari Hiroto <henl16101118@gmail.com>
2020-08-10 15:52:04 +0900

    Update application

--- db/migrate/20200708032306_create_users.rb
+++ db/migrate/20200708032306_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :neme
       t.string :email
       t.string :image_name
 
