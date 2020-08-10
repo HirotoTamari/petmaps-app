e78aed942beb8be33caa131966b5f3b100424797
Tamari Hiroto <henl16101118@gmail.com>
2020-08-10 15:11:18 +0900

    Update application

--- db/migrate/20200730074732_change_users_columns.rb
+++ db/migrate/20200730074732_change_users_columns.rb
@@ -2,7 +2,6 @@ class ChangeUsersColumns < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :password_digest, :string
     remove_column :users, :password, :string
-    remove_column :users, :neme, :string
     remove_column :users, :route1, :string
     remove_column :users, :route2, :string
     remove_column :users, :route3, :string
