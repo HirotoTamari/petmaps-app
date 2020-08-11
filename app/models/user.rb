abecb4c3228fa14eb59c3efc5c41398adb500fed
Tamari Hiroto <henl16101118@gmail.com>
2020-08-11 23:58:40 +0900

    Update application

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,29 +5,29 @@ class User < ApplicationRecord
     validates :name, {presence: true}
     validates :email, {presence: true, uniqueness: true}
 
-    devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable,
-        :omniauthable, omniauth_providers: %i[facebook google_oauth2]
+    # devise :database_authenticatable, :registerable,
+    #     :recoverable, :rememberable, :validatable,
+    #     :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
-        def self.find_for_oauth(auth)
-            user = User.where(uid: auth.uid, provider: auth.provider).first
+    #     def self.find_for_oauth(auth)
+    #         user = User.where(uid: auth.uid, provider: auth.provider).first
         
-            unless user
-                user = User.create(
-                    uid:      auth.uid,
-                    provider: auth.provider,
-                    name:     auth.info.name,
-                    email:    User.dummy_email(auth),
-                    password: Devise.friendly_token[0, 20]
-                )
-            end
+    #         unless user
+    #             user = User.create(
+    #                 uid:      auth.uid,
+    #                 provider: auth.provider,
+    #                 name:     auth.info.name,
+    #                 email:    User.dummy_email(auth),
+    #                 password: Devise.friendly_token[0, 20]
+    #             )
+    #         end
         
-            user
-        end
+    #         user
+    #     end
         
-        private
+    #     private
         
-        def self.dummy_email(auth)
-            "#{auth.uid}-#{auth.provider}@example.com"
-        end
+    #     def self.dummy_email(auth)
+    #         "#{auth.uid}-#{auth.provider}@example.com"
+    #     end
 end
