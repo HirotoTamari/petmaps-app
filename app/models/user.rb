e7ac5dd09552e3a890c1005f74ab899032224644
Tamari Hiroto <henl16101118@gmail.com>
2020-08-14 16:40:51 +0900

    Update application

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,4 @@ class User < ApplicationRecord
     #0文字になればヴァリデーション
     validates :name, {presence: true}
     validates :email, {presence: true, uniqueness: true}
-
-    has_many :sns_credentials, dependent: :destroy
-
-    devise :database_authenticatable, :registerable,
-            :recoverable, :rememberable, :validatable,
-            :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 end
