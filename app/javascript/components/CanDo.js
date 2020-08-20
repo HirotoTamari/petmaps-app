7c4231ca626469ee0ccf7561c74538108b03bd56
Tamari Hiroto <henl16101118@gmail.com>
2020-08-20 10:09:44 +0900

    Update application

---
+++ app/javascript/components/CanDo.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CanDo extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {isModalOpen: false};
+    }
+    
+    handleClickCando() {
+        this.setState({isModalOpen: true});
+    }
+    
+    handleClickClose(){
+        this.setState({isModalOpen: false});
+    }
+
+    render(){
+        let modal;
+        if (this.state.isModalOpen) {
+            modal = (
+                <div className='modal' onClick={() => {this.handleClickClose()}}>
+                    <div className='modal_inner'>
+                        <div className="modal_mov">
+                        <video autoPlay loop src={this.props.mov}></video>
+                        </div>
+                        <div className="modal_title">
+                            <h2>{this.props.name}</h2>
+                        </div>
+                        <div className="modal_content">
+                            <p>{this.props.content}</p>
+                        </div>
+                        <div className="close_button" onClick={() => {this.handleClickClose()}}>
+                            <img src="/close_button.png" />
+                        </div>
+                    </div>
+                </div>
+                );
+            }
+        return(
+            <div
+                onClick={() => {
+                this.handleClickCando()
+                }}>
+                <img src={this.props.image} />
+                <div className="nav_item_title_1">
+                    <div className="item_padding_1">
+                        {this.props.name}
+                    </div>
+                </div>
+                {modal}
+            </div>
+        );
+    }
+}
+
+export default CanDo;
